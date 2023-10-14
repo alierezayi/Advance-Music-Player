48b047be978c92370b80aca60468e7df78e07673
Ali Rezaei <im1.bitcode@gmail.com>
2023-10-14 21:00:55 +0330

    layout / basic header / Button - Header *components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,11 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">
-      hello (site)!
+    <div className=" bg-neutral-900 rounded-lg h-full min-h-[675px] w-full overflow-hidden overflow-y-auto">
+      <Header>
+        
+      </Header>
     </div>
-  )
+  );
 }
