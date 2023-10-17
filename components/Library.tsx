1d3ae57f732e61fd42f8f6fc5991c57e4a4d6c57
Ali Rezaei <im1.bitcode@gmail.com>
2023-10-17 11:28:40 +0330

    database / uoload song - image / modal and functionallity

--- components/Library.tsx
+++ components/Library.tsx
@@ -3,8 +3,21 @@
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
 
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
   const onClick = () => {
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    return uploadModal.onOpen();
     // handle upload later
   };
 
@@ -23,9 +36,7 @@ const Library = () => {
         />
       </div>
 
-      <div className="flex flex-col gap-y-2 mt-4 px-3">
-        List of Songs!
-      </div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
     </div>
   );
 };
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -18,6 +19,8 @@ const ModalProvider = () => {
   return (
     <>
       <AuthModal />
+
+      <UploadModal />
     </>
   );
 };
