80925e45f79c8cd78c8055488eb98c7dfbf36df7
Ali Rezaei <im1.bitcode@gmail.com>
2023-10-18 14:29:47 +0330

    favorites / functionality - page

--- app/search/components/SearchContent.tsx
+++ app/search/components/SearchContent.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LikedButton from "@/components/LikedButton";
 import MediaItem from "@/components/MediaItem";
 
 import { Song } from "@/types";
@@ -24,6 +25,8 @@ const SearchContent: React.FC<SearchContentProps> = ({ songs }) => {
           <div className="flex-1">
             <MediaItem onClick={() => {}} data={song} />
           </div>
+
+          <LikedButton songId={song.id} />
         </div>
       ))}
     </div>
